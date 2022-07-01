64b9fdcf9b2b0cc2c9a081f644ab2e1531019810
azizyelbay <34959497+azizyelbay@users.noreply.github.com>
2022-07-01 19:50:15 +0300

    Added notification-service.

---
+++ notification-service/src/main/java/com/assignment/notificationservice/model/Notification.java
@@ -0,0 +1,25 @@
+package com.assignment.notificationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="notifications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long advertisementId;
+    private String message;
+    private LocalDateTime createdAt;
+    private String state;
+
+}
